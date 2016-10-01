67fd8c324463b7cc0d56aeb4ff975ca08379e0b1
nmsutton <Nate Sutton>
2016-10-01 18:24:31 -0400

    added neural signal calibrator work in progress.

--- src/owSignalCalibrator.py
+++ src/owSignalCalibrator.py
@@ -1,38 +1,63 @@
-from pylab import *;
+'''
+*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011, 2013 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************
 
-# Basic parameters.
-W=10;
-Z=0.02;
-# Initial time.
-t0 = 0;
-# End time.
-t1 = 0.5;
-# Time vector with 200 points.
-npts=200;
-t=linspace(t0,t1,npts);
+Methods are included here to calibrate neural signals into input which
+creates appropriate actions in sibernetic worm model movements.
 
-# General analytical solution.
-V = -10.001*exp(-t/Z)+W;
-
+Author: Nate Sutton
+Copyright 2016
+'''
 
 import numpy as np
 import scipy.integrate as de
 import matplotlib.pyplot as plt
 
-W=10;
-Z=0.02;
-V2 = -10.001
+# Initalization parameters
+V = -10.001 # inital voltage
+W = 10; # target voltage
+Z = 0.02; # tau, speed limiting factor
 x = np.linspace(0,0.5,200) # time points
 
 class single_neuron():
-	def __init__(neuron, V, W, Z, lag_time):
+	def __init__(neuron, V, W, Z, x, lag_time):
 		neuron.V = V		
 		neuron.W = W
 		neuron.Z = Z
-		neuron.x = np.linspace(0,0.5,200)
+		neuron.x = x
 		neuron.refrac_delay = 120#0.1
 		neuron.refrac_current = 0.0
-		neuron.refrac_toggle = 0
+		neuron.refrac_toggle = False
 		neuron.refrac_trigger = 9.9
 		neuron.reset_voltage = -10.0
 		neuron.lag_time = lag_time
@@ -45,9 +70,10 @@ class single_neuron():
 		neuron.i += 1
 
 	def g(neuron, V, x):
+		'''
+		Equations for neural spiking
+		'''
 		V = neuron.refrac_test(V)
-		#V = neuron.V
-		#print(V)
 
 		if (neuron.lag_finished()):
 			V = (-V+neuron.W)/neuron.Z
@@ -64,9 +90,6 @@ class single_neuron():
 		if neuron.lag_counter > neuron.lag_time:
 			lag_over = True
 
-		#print(neuron.lag_counter)
-		#print(neuron.lag_time)
-
 		return lag_over
 
 	def neuron_out(neuron):
@@ -80,22 +103,21 @@ class single_neuron():
 		neuron.W, neuron.Z, neuron.refrac_delay, \
 		neuron.refrac_current, neuron.refrac_toggle, \
 		neuron.refrac_trigger, neuron.reset_voltage
-		#print(V)
+
 		#refractory point
 		if V >= refrac_trigger:
 			V = reset_voltage
 
-			refrac_toggle = 1
+			refrac_toggle = True
 
-		if refrac_toggle == 1:
+		if refrac_toggle == True:
 			W = 0
-			refrac_current += 1#0.1/200.0
-			#print(refrac_current)
+			refrac_current += 1
 
 			if refrac_current >= refrac_delay: 
 				W = 10 	
 				refrac_current = 0.0
-				refrac_toggle = 0  		
+				refrac_toggle = False 		
 
 		neuron.W, neuron.Z, neuron.refrac_delay, \
 		neuron.refrac_current, neuron.refrac_toggle, \
@@ -105,53 +127,13 @@ class single_neuron():
 
 		return V		
 
-'''
-sn = single_neuron()
-sn.print_i()
-sn.iter_i()
-sn.iter_i()
-sn.print_i()
-'''
-
-# Initial conditions on y, y' at x=0
-#init = V2#W, Z, V2, refrac_delay, refrac_current, refrac_toggle
-# First integrate from 0 to 2
-
-#sol=de.odeint(g, init, x)
-#plt.plot(x, sol[:,0], t, V)
 lag_time = 0.0
-sn = single_neuron(V2, W, Z, lag_time)
+sn = single_neuron(V, W, Z, x, lag_time)
 no = sn.neuron_out()
 
 lag_time = 23.0
-sn_2 = single_neuron(V2, W, Z, lag_time)
+sn_2 = single_neuron(V, W, Z, x, lag_time)
 no_2 = sn_2.neuron_out()
 
-#print(no)
-#print(len(no))
-#print(len(x))
-#plt.plot(x, no)
-#plt.plot(x, no, t, V)
-#plt.plot(x, no[:,0], t, V)
-#plt.plot(x, no[:,0])
 plt.plot(x, no[:,0], x, no_2[:,0])
 plt.show()
-
-'''
-# Preallocation.
-Vn = zeros(t.shape);
-# Initial conditions.
-Vn[0] = -0.001; 
-# Now do the explicit Euler integration.
-for ij in range(1,len(t)):
-	# Delta t.
-	dt = t[ij]-t[ij-1];
-	# Slope.
-	dV_dt = (-Vn[ij-1]+W)/Z;
-	Vn[ij]=Vn[ij-1] + dV_dt*dt;
-
-# First solution.
-#plot(t,V,t,Vn);
-plot(t,V);
-show();
-'''
